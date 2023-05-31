bbe785b87578e729134d5012b3434134e3551816
Kseniya Polomoshnova <kseniia.polomoshnova@yandex.ru>
2023-05-31 13:53:30 +0700

    task 4 done

--- task_4/solution.js
+++ task_4/solution.js
@@ -4,7 +4,6 @@ function parseProducts(json) {
     let data = JSON.parse(json);
     let products = data.products;
     return products;// Аргументом функции является JSON
-
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
@@ -13,8 +12,7 @@ function parseProducts(json) {
 
 function renderProductsCards(json) {
     clearProducts();
-    let products;
-    products = parseProducts(json);
+    let products = parseProducts(json);
     let length = products.length;
     for (let i = 0; i < length; i += 1) {
         addProduct(products[i]);
