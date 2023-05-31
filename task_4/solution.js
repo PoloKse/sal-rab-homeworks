4c628048e82e35c055a5c34b2abc0e6c03d8fc2d
Kseniya Polomoshnova <kseniia.polomoshnova@yandex.ru>
2023-05-31 13:45:46 +0700

    task 4 done

--- task_4/solution.js
+++ task_4/solution.js
@@ -1,10 +1,8 @@
 // Задание 4.1. Вывод карточек товаров
 
 function parseProducts(json) {
-    let data;
-    data = JSON.parse(json);
-    let products;
-    products = data.products;
+    let data = JSON.parse(json);
+    let products = data.products;
     return products;// Аргументом функции является JSON
 
     // Преобразуйте строку json, переданную как аргумент функции,
@@ -15,10 +13,9 @@ function parseProducts(json) {
 
 function renderProductsCards(json) {
     clearProducts();
-    renderProductsCards(products);
     let products;
     products = parseProducts(json);
-    length = products.length;
+    let length = products.length;
     for (let i = 0; i < length; i += 1) {
         addProduct(products[i]);
      }
